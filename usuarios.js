4a1f40a6fea6a6d96f9f1ee6ad6e06a9aab9b67c
ICZAS PIERRE <izaiaspierry@gmail.com>
2022-10-26 13:17:26 -0300

    como nao ta indoo?

--- usuarios.js
+++ usuarios.js
@@ -1,1 +1,18 @@
-//Arquivo principal
+
+
+
+
+
+
+
+const usuarios = require('./database/usuarios_digital_house/usuarios.json');
+
+const removerUsuario = (idUser) => {
+    let usuarioIndex = usuarios.findIndex((usuario) => usuario.Index == idUser)
+    if (usuarioIndex > -1){
+        usuarios.splice(usuarioIndex, 1)
+    }
+
+};
+    removerUsuario(1);
+    console.log(usuarios);
